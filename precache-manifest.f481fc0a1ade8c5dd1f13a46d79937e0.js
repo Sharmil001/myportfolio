474ac4934510901755b7abb4d00cd05f9bc3929a
Sharmil <sharmil@Sharmils-MacBook-Air.local>
2021-06-15 13:26:00 +0530

    Updates

---
+++ precache-manifest.f481fc0a1ade8c5dd1f13a46d79937e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e108a43f06bf49827abe129218f8b17",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "40b27f206d935ec702d6",
+    "url": "/myportfolio/static/css/main.a988531f.chunk.css"
+  },
+  {
+    "revision": "939fad081115a7df447e",
+    "url": "/myportfolio/static/js/2.cbd3fe96.chunk.js"
+  },
+  {
+    "revision": "4c1abe8f7ce1e5ec185ecd843345149c",
+    "url": "/myportfolio/static/js/2.cbd3fe96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b27f206d935ec702d6",
+    "url": "/myportfolio/static/js/main.1508361d.chunk.js"
+  },
+  {
+    "revision": "c6c7faf1e982ab005e8b",
+    "url": "/myportfolio/static/js/runtime-main.e3b96885.js"
+  },
+  {
+    "revision": "72cde5813503aff2994bcdbd00dea887",
+    "url": "/myportfolio/static/media/down-scroll.72cde581.svg"
+  },
+  {
+    "revision": "2ecb80db42c30fb77ff200b22606c141",
+    "url": "/myportfolio/static/media/logo-full.2ecb80db.svg"
+  },
+  {
+    "revision": "e9166ef2f8c07b54c76342d8c5e4920c",
+    "url": "/myportfolio/static/media/triangle.e9166ef2.svg"
+  }
+]);
